bba728cb0a16e14d1b413f397799e2354989266b
Ian Bell <ian.bell@nist.gov>
2023-01-18 13:14:13 -0500

    This time the fitting script

--- fit_models.py
+++ fit_models.py
@@ -1,6 +1,5 @@
 import os, timeit, json, uuid, shutil, random, time
 from itertools import repeat, cycle, islice
-from multiprocessing import current_process
 
 def roundrobin(*iterables):
     "roundrobin('ABC', 'D', 'EF') --> A D E B F C"
@@ -19,11 +18,12 @@ def roundrobin(*iterables):
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize
+import pandas
 
 import teqp
 print(f'teqp version: {teqp.__version__}')
 
-from temo.fit import mutant_factories, data_loaders, cost_contributions
+from temo.fit import mutant_factories, data_loaders, cost_contributions, data_transforms
 
 class DataVault():
 
@@ -31,24 +31,28 @@ class DataVault():
     step_PVT_P = 10
     step_SOS = 10
     step_VLE = 40
+    step_B12 = 1
 
     def __init__(self, dataroot, job, *, teqp_data_root):
         """
         Initialize the data vault which contains the experimental
         data in pandas DataFrames, and builds some base models needed
-        for other calculation
+        for other calculations
         """
 
         self.names = job['names']
         molar_masses = job['molar_mass / kg/mol']
-        abbrevs = ['PVT', 'PVT_P', 'SOS', 'VLE', 'CRIT'] # shorthand for the kinds of data, just to simplify what follows
+        abbrevs = ['PVT', 'VLE', 'SOS'] # shorthand for the kinds of data, just to simplify what follows
+         # other options are: 'PVT_P', 'CRIT', 'B12'
         for abbrev in abbrevs:
-            load_func = getattr(data_loaders, 'load_' + abbrev)
+            load_func = getattr(data_loaders, 'load_' + abbrev) # This dynamically obtains the function; something like : data_loaders.load_PVT
+            # And then the function pointer is called to actually load the data
             df = load_func(dataroot, identifier='FLD', identifiers=self.names, molar_masses=molar_masses)
-            setattr(self, 'df_'+abbrev, df) # makes an attribute like: self.df_PVT
+            setattr(self, 'df_'+abbrev, df) # makes an attribute like: self.df_PVT within this instance
 
         # Build the model (here used to hold the pure fluid information)
         self.model = teqp.build_multifluid_model(self.names, teqp_data_root, teqp_data_root+'/dev/mixtures/mixture_binary_pairs.json', {'estimate':'Lorentz-Berthelot'})
+        self.pures = [teqp.build_multifluid_model([name], teqp_data_root, teqp_data_root+'/dev/mixtures/mixture_binary_pairs.json') for name in self.names]
 
     def cost_function(self, *, params, mutant=None, mutant_factory=None, mutant_kwargs={}):
         """ The cost function to be minimized
@@ -74,11 +78,11 @@ class DataVault():
         time_PVT = timeit.default_timer()-tic
 
         costrho_P = 0
-        tic = timeit.default_timer()
-        errrho_P = costs.calc_errrho_devp(df=self.df_PVT_P, model=mutant, step=self.step_PVT_P)
-        errrho_P *= get_weight(self.df_PVT_P)
-        costrho_P = np.abs(errrho_P).mean()
-        time_PVT_P = timeit.default_timer()-tic
+        # tic = timeit.default_timer()
+        # errrho_P = costs.calc_errrho_devp(df=self.df_PVT_P, model=mutant, step=self.step_PVT_P)
+        # errrho_P *= get_weight(self.df_PVT_P)
+        # costrho_P = np.abs(errrho_P).mean()
+        # time_PVT_P = timeit.default_timer()-tic
 
         costSOS = 0
         tic = timeit.default_timer()
@@ -103,9 +107,11 @@ class DataVault():
         # costcrit = np.abs(errcrit).mean()
 
         costcritisoT = 0
-        errcrit = costs.calc_err_critisoT(df=self.df_CRIT, model=mutant, step=5)
-        errcrit *= get_weight(self.df_CRIT)
-        costcritisoT = np.abs(errcrit).mean()*10
+        # tic = timeit.default_timer()
+        # errcrit = costs.calc_err_critisoT(df=self.df_CRIT, model=mutant, step=1)
+        # errcrit *= get_weight(self.df_CRIT)
+        # costcritisoT = np.abs(errcrit).mean()
+        # time_critisoT = timeit.default_timer()-tic
 
         costcritpts = 0
         # errcrit = costs.calc_errcritPT(df=self.df_CRIT, model=mutant)
@@ -113,13 +119,14 @@ class DataVault():
         # costcritpts = np.abs(errcrit).mean()*10
 
         costB12 = 0
-        # errB12 = self.calc_errB12(self.df_B12, model=mutant, step=1, z0 = 0.5)
-        # costB12 = np.abs(errB12).mean()*1000*1000
+        # for z0 in [0.2, 0.5, 0.8]:
+        #     errB12 = costs.calc_errB12(df=self.df_B12, model=mutant, step=1, z0=z0)*1e6 # signed difference in cm^3/mol
+        #     errB12 *= get_weight(self.df_B12)
+        #     costB12 += np.abs(errB12).mean()
 
-        # print(f'[timing] mutant: {time_mutant} PVT: {time_PVT} PVT_P: {time_PVT_P} SOS: {time_SOS} VLE: {time_VLE}')
+        # print(f'[timing] mutant: {time_mutant} PVT: {time_PVT} PVT_P: {time_PVT_P} SOS: {time_SOS} VLE: {time_VLE} CRIT(T): {time_critisoT}')
 
-        cost = 4*costrho + costrho_P + 1*costSOS + 1*costVLE + costB12 + costcritpts + costcrit + costcritisoT
-        # cost = costcrit
+        cost = 4*costrho + costrho_P + 1*costSOS + 1*costVLE + 10*costB12 + costcritpts + costcrit + 0.5*costcritisoT
 
         return cost
 
@@ -147,7 +154,7 @@ def do_fit(FLDs, deptype, Ndep, root, *, Nrep=1, dv, mutant_kwargs={}):
         Ngaussian = Ndep-Npoly
         bounds = ([(0.75,1.25)]*4 # betaT,gammaT,betaV,gammaV
                 + [(-3.0,3.0)]*Ndep # n
-                + [(-3,8)]*Ndep  # t
+                + [(-1,8)]*Ndep  # t
                 + [(0,3)]*(2*Ngaussian) # eta, beta
                 + [(0,3)]*(2*Ngaussian) # gamma, epsilon
                 )
@@ -182,19 +189,24 @@ def do_fit(FLDs, deptype, Ndep, root, *, Nrep=1, dv, mutant_kwargs={}):
                     mutant = mutant_factory(model=self.basemodel, params=x, **mutant_kwargs)
                     mutant_json = json.loads(mutant.get_meta())
                     AAD_PVT = np.mean(np.abs(cost_contributions.calc_errrho(model=mutant, df=dv.df_PVT, step=dv.step_PVT, iterate=True)))
-                    AAD_PVT_P = np.mean(np.abs(cost_contributions.calc_errrho_devp(model=mutant, df=dv.df_PVT_P, step=dv.step_PVT_P)))
+                    # AAD_PVT_P = np.mean(np.abs(cost_contributions.calc_errrho_devp(model=mutant, df=dv.df_PVT_P, step=dv.step_PVT_P)))
                     AAD_SOS = np.mean(np.abs(cost_contributions.calc_errSOS(model=mutant, df=dv.df_SOS, step=dv.step_SOS, max_iter=10)))
                     AAD_VLE = np.mean(np.abs(cost_contributions.calc_errVLE(model=mutant, df=dv.df_VLE, step=dv.step_VLE)))
-                    AAD_CRIT_ISOT = np.mean(np.abs(cost_contributions.calc_err_critisoT(model=mutant, df=dv.df_CRIT, step=5)))
-                    print(f"AAD(PVT): {AAD_PVT:0.2f} %; AAD(PVT_P): {AAD_PVT_P:0.2f} %; AAD(SOS): {AAD_SOS:0.2f} %; AAD(VLE): {AAD_VLE:0.2f} %; AAD(CRIT_ISO_T): {AAD_CRIT_ISOT:0.2f} %")
+                    # AAD_CRIT_ISOT = np.mean(np.abs(cost_contributions.calc_err_critisoT(model=mutant, df=dv.df_CRIT, step=1)))
+                    # print(f"AAD(PVT): {AAD_PVT:0.2f} %; AAD(PVT_P): {AAD_PVT_P:0.2f} %; AAD(SOS): {AAD_SOS:0.2f} %; AAD(VLE): {AAD_VLE:0.2f} %; AAD(CRIT_ISO_T): {AAD_CRIT_ISOT:0.2f} %")
+                    print(f"AAD(PVT): {AAD_PVT:0.2f} %; AAD(SOS): {AAD_SOS:0.2f}; AAD(VLE): {AAD_VLE:0.2f} %")
                     with open(f'{root}/{uid}_step{self.step_number}.json', 'w') as fp:
                         fp.write(json.dumps({
+                            'step_number': self.step_number,
                             'x': x,
                             'cost': objective(x),
                             'convergence': convergence,
                             'model': mutant_json,
                             'AAD(PVT, iterate) / %': AAD_PVT,
                             'AAD(SOS) / %': AAD_SOS,
+                            # 'AAD(PVT_P)': AAD_PVT_P,
+                            'AAD(VLE)': AAD_VLE,
+                            # 'AAD(crit(T))': AAD_CRIT_ISOT
                         }))
                 except BaseException as be:
                     print(be)
@@ -219,9 +231,32 @@ def do_fit(FLDs, deptype, Ndep, root, *, Nrep=1, dv, mutant_kwargs={}):
                                  'x': res.x.tolist(), 'elapsed / s':toc-tic}))
 
 if __name__ == '__main__':
+
     import CoolProp.CoolProp as CP
-    pairs = [('AMMONIA', 'WATER'),]
-    dataroot = 'NH3H2O'
+
+    pairs = [('R32', 'R1234YF'),]
+    dataroot = 'data/JPCRD2023'
+
+    os.environ['RPPREFIX'] = os.getenv('HOME') + '/REFPROP10'
+    
+    from ctREFPROP.ctREFPROP import REFPROPFunctionLibrary
+    RP = REFPROPFunctionLibrary(os.getenv('RPPREFIX'))
+    RP.SETPATHdll(os.getenv('RPPREFIX'))
+    RP.SETFLUIDSdll('R32*R1234YF')
+
+    # Add the ideal-gas contribution to the speed of sound data file and the first guess for density
+    # Note: ensure that teqp and REFPROP are using the same pure fluid EOS, otherwise
+    #       your calculated SOS will be in error
+    df = pandas.read_csv(dataroot+'/raw/SOS_raw.csv', comment='#')
+    df = data_transforms.add_Ao20_REFPROP(df, RP=RP)
+    df = data_transforms.add_homogeneous_density_REFPROP(df, RP=RP)
+    df.to_csv(dataroot+'/SOS.csv', index=False)
+
+    # Similar thing with VLE, put in starting molar concentrations for the iterative calculations
+    df = pandas.read_csv(dataroot+'/raw/VLE_raw.csv', comment='#')
+    df = data_transforms.add_coexisting_concentrations_REFPROP(df, RP=RP)
+    df.to_csv(dataroot+'/VLE.csv', index=False)
+
     def get_dv_args(FLDs):
         job = {
             'names': FLDs,
@@ -238,32 +273,37 @@ if __name__ == '__main__':
     print(f'Starting optimization in {root}')
     shutil.copy2(__file__, root)
     shutil.copytree(dataroot, root + '/fitdataroot')
-    
-    # Serial
-    Ndep = 6
-    Npoly = Ndep
-    dvals = [1,2,3,4,5,6]
-    d = list(roundrobin(*repeat(dvals, 2)))[0:Ndep] # [1,1,2,2,3,3...]
-    kwargs = dict(Npoly=Npoly, Ngaussian=Ndep-Npoly, d=d, l=[1]*Ndep)
-    do_fit(pairs[0], 'Gaussian+Exponential', Ndep, root, dv=dv, mutant_kwargs=kwargs)
 
-    # # Call the spawner to fit in parallel
-    # deptype = 'Gaussian+Exponential'
-    # from spawn import Spawner
-    # args = []
-    # for pair in pairs:
-    #     for Ndep in range(6):
-    #         for Npoly in [Ndep]:#range(Ndep+1):
-    #             for N in range(2): # number of repeats
-    #                 Ndep = Npoly
-    #                 # Npoly = Ndep
-    #                 dvals = [1,2,3,4,5,6]
-    #                 d = list(roundrobin(*repeat(dvals, 2)))[0:Npoly] # [1,1,2,2,3,3...]
-    #                 kwargs = dict(Npoly=Npoly, Ngaussian=Ndep-Npoly, d=d, l=[1]*Npoly)
-    #                 datavault_kwargs = get_dv_args(pair)
-    #                 args.append(dict(target=do_fit, args = [pair, deptype, Ndep, root], kwargs=dict(dv=datavault_kwargs, mutant_kwargs=kwargs)))
-    # spawner = Spawner(args, Nproc_max=12)
-    # print(spawner.run())
+    # Can be changed to false to run multiple fits in parallel
+    serial = True
+    
+    if serial:
+        # Serial
+        Ndep = 3
+        Npoly = Ndep
+        dvals = [1,2,3,4,5,6]
+        d = list(roundrobin(*repeat(dvals, 3)))[0:Ndep] # [1,1,2,2,3,3...]
+        kwargs = dict(Npoly=Npoly, Ngaussian=Ndep-Npoly, d=d, l=[1,1,1,1,2])
+        do_fit(pairs[0], 'Gaussian+Exponential', Ndep, root, dv=dv, mutant_kwargs=kwargs)
+    else:
+        # Call the spawner to fit in parallel
+        deptype = 'Gaussian+Exponential'
+        from spawn import Spawner
+        args = []
+        for pair in pairs:
+            for Ndep in range(3,6):
+                for Npoly in [Ndep]:#range(Ndep+1):
+                    for N in range(4): # number of repeats
+                        Ndep = Npoly
+                        # Npoly = Ndep
+                        dvals = [1,2,3,4,5,6]
+                        d = list(roundrobin(*repeat(dvals, 3)))[0:Npoly] # [1,1,2,2,3,3...]
+                        lvals = cycle([1, 1, 2])
+                        kwargs = dict(Npoly=Npoly, Ngaussian=Ndep-Npoly, d=d, l=[next(lvals) for _ in range(Npoly)])
+                        datavault_kwargs = get_dv_args(pair)
+                        args.append(dict(target=do_fit, args = [pair, deptype, Ndep, root], kwargs=dict(dv=datavault_kwargs, mutant_kwargs=kwargs)))
+        spawner = Spawner(args, Nproc_max=16)
+        print(spawner.run())
 
     print(f'Finished optimization in {root}')
     shutil.make_archive(root, format='xztar', root_dir=root)
