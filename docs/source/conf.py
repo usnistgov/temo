fb38e23cb04800fd939023d4e9cb3271e2491eda
Ian Bell <ian.bell@nist.gov>
2023-04-26 13:09:38 -0400

    Don't convert the converted docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ if on_rtd:
     # subprocess.check_output(f'jupyter nbconvert --version', shell=True)
     for path, dirs, files in os.walk('.'):
         for file in files:
-            if file.endswith('.ipynb') and '.ipynb_checkpoints' not in path:
+            if file.endswith('.ipynb') and '.ipynb_checkpoints' not in path and '.nbconvert.' not in path:
                 import glob; print(glob.glob(f'{path}/*.*'))
                 subprocess.check_output(f'jupyter nbconvert  --to notebook --output {file} --execute {file}', shell=True, cwd=path)
                 # --ExecutePreprocessor.allow_errors=True      (this allows you to allow errors globally, but a raises-exception cell tag is better)
