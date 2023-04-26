bbf493d7a16be51dccd1f2ee5aed0747f039cd47
Ian Bell <ian.bell@nist.gov>
2023-04-26 12:18:02 -0400

    Add temo to path in a better way

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,8 @@ on_rtd = True
 #
 # import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+here = os.path.dirname(__file__)
+sys.path.insert(0, os.path.abspath(here+'/../..'))
 
 
 # -- Project information -----------------------------------------------------
